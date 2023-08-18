3e356e99179562540e08f7b90cfa640ee18764b5
Nenad Horvat <horvat.nenad02@gmail.com>
2023-08-18 10:25:18 +0200

    Added New User form & all Users datatable

--- Universal/Admin-Controllers/AdminMVC/DashboardController.cs
+++ Universal/Admin-Controllers/AdminMVC/DashboardController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Universal.DTO.ViewDTO;
 
 namespace Universal.Admin_Controllers.AdminMVC
 {
@@ -7,6 +8,29 @@ namespace Universal.Admin_Controllers.AdminMVC
 		public IActionResult Index()
 		{
 			return View("Home");
+		}
+
+		public IActionResult TEMPUSER()
+		{
+			return View("../User/NewUser", new UserIDTO());
+		}
+
+        public IActionResult TEMPallUSER()
+        {
+            return View("../User/Users");
+        }
+
+        public IActionResult TEMPnewUSER(UserIDTO userIDTO)
+		{
+			if(!ModelState.IsValid) return View("../User/NewUser", new UserIDTO());
+
+			var filePath = Path.Combine("wwwroot\\images\\", userIDTO.Avatar.FileName);
+			using (FileStream fs = System.IO.File.Create(filePath))
+			{
+				userIDTO.Avatar.CopyTo(fs);
+			}
+
+			return View("../User/Users");
 		}
 	}
 }
---
+++ UniversalDTO/ViewDTO/UserIDTO.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Universal.DTO.ViewDTO
+{
+	public class UserIDTO
+	{
+		[Required]
+		public int RoleId { get; set; }
+		public List<(int Id, string Name)> Role { get; set; } = new List<(int Id, string Name)>() { (1, "User"), (2, "Admin") };
+		[Required]
+		public string Email { get; set; }
+		[Required]
+		public string FirstName { get; set; }
+		[Required]
+		public string LastName { get; set; }
+		public string? Phone { get; set; }
+		public string? Address { get; set; }
+		public string? City { get; set; }
+		public string? Zip { get; set; }
+		public string? Country { get; set; }
+		[Required]
+		public string Password { get; set; }
+		[Required]
+		public IFormFile Avatar { get; set; }
+	}
+}
